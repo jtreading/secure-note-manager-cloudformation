7c3211ff091f45cbbe504d6f88b5dfafaa38cca7
Jonathon Reading <jonathonreading@gmail.com>
2024-02-07 15:49:30 -0700

    adds build to create script

--- Scripts/python/deploy_stacks.py
+++ Scripts/python/deploy_stacks.py
@@ -7,7 +7,7 @@ import pprint
 
 def CreateStack(stack_name, template_file, parameters, aws_profile, aws_region, resource_locators):
     # Create the CloudFormation stack with parameters
-    print("Creating CloudFormation stack...")
+    print(f"Creating {stack_name}.")
     client = boto3.client('cloudformation', region_name=aws_region)
     os.environ['AWS_PROFILE'] = aws_profile
     response = client.create_stack(
@@ -28,9 +28,12 @@ def CreateStack(stack_name, template_file, parameters, aws_profile, aws_region, 
     if stack_status == "CREATE_COMPLETE":
         print("Stack creation completed successfully.")
 
-        stack_outputs = response['Stacks'][0]['Outputs']
+        if 'Outputs' in response['Stacks'][0]:
+            stack_outputs = response['Stacks'][0]['Outputs']
+        else:
+            stack_outputs = []
         for output in stack_outputs:
-            resource_locators[output['OutputKey']] = output['OutputValue']
+            resource_locators[output['ExportName']] = output['OutputValue']
 
         print("Resource locators:")
         pprint.pprint(resource_locators)
@@ -65,7 +68,6 @@ def SyncRepository(github_repo_url, codecommit_repo_url):
 
     print("GitHub to CodeCommit sync completed.")
 
-
 def CreateNetworkStack(config_data, resource_locators):
     # Configuration
     AWS_PROFILE = "default"
@@ -74,11 +76,13 @@ def CreateNetworkStack(config_data, resource_locators):
     TEMPLATE_FILE = "../../Config/network.yml"
 
     # Parameters
+    AppName = config_data.get('AppName')
     ProjectName = config_data.get('ProjectName')
     Environment = config_data.get('Environment')
     IngressCIDR = f'{config_data.get('IngressIP')}/32'
 
     PARAMETERS = [
+        {"ParameterKey": "AppName", "ParameterValue": AppName},
         {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
         {"ParameterKey": "Environment", "ParameterValue": Environment},
         {"ParameterKey": "IngressCIDR", "ParameterValue": IngressCIDR}
@@ -106,23 +110,107 @@ def CreateServiceRolesStack(config_data, resource_locators):
 
     return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
 
+def CreateAPIBuildStack(config_data, resource_locators):
+    # Configuration
+    AWS_PROFILE = "default"
+    AWS_REGION = "us-east-1"
+    STACK_NAME = f"{config_data.get('AppName')}-api-build-stack"
+    TEMPLATE_FILE = "../../Pipelines/build.yml"
+
+    # Parameters
+    AppName = f"{config_data.get('AppName')}-api"
+    ProjectName = config_data.get('ProjectName')
+    Environment = config_data.get('Environment')
+        
+    CodeCommitRepo = resource_locators.get(f'{AppName}-cc-repository-url')
+    DockerHubUsername = input("Enter DockerHub username: ")
+    DockerHubPassword = input("Enter DockerHub password: ")
+    CodeBuildServiceRole = resource_locators.get(f'{config_data.get('AppName')}-codebuild-service-role-arn')
+    ECRRepositoryURL = resource_locators.get(f'{AppName}-ecr-repository-url')
+    ECRRepositoryName = ECRRepositoryURL.split("/")[-1]
+
+    PARAMETERS = [
+        {"ParameterKey": "AppName", "ParameterValue": AppName},
+        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
+        {"ParameterKey": "Environment", "ParameterValue": Environment},
+        {"ParameterKey": "AWSRegion", "ParameterValue": AWS_REGION},
+        {"ParameterKey": "CodeCommitRepo", "ParameterValue": CodeCommitRepo},
+        {"ParameterKey": "DockerHubUsername", "ParameterValue": DockerHubUsername},
+        {"ParameterKey": "DockerHubPassword", "ParameterValue": DockerHubPassword},
+        {"ParameterKey": "CodeBuildServiceRole", "ParameterValue": CodeBuildServiceRole},
+        {"ParameterKey": "ECRRepositoryURL", "ParameterValue": ECRRepositoryURL},
+        {"ParameterKey": "ECRRepositoryName", "ParameterValue": ECRRepositoryName}
+    ]
+
+    return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
+
+def CreateUIBuildStack(config_data, resource_locators):
+    # Configuration
+    AWS_PROFILE = "default"
+    AWS_REGION = "us-east-1"
+    STACK_NAME = f"{config_data.get('AppName')}-ui-build-stack"
+    TEMPLATE_FILE = "../../Pipelines/build.yml"
+
+    # Parameters
+    AppName = f"{config_data.get('AppName')}-ui"
+    ProjectName = config_data.get('ProjectName')
+    Environment = config_data.get('Environment')
+        
+    CodeCommitRepo = resource_locators.get(f'{AppName}-cc-repository-url')
+    DockerHubUsername = input("Enter DockerHub username: ")
+    DockerHubPassword = input("Enter DockerHub password: ")
+    CodeBuildServiceRole = resource_locators.get(f'{config_data.get('AppName')}-codebuild-service-role-arn')
+    ECRRepositoryURL = resource_locators.get(f'{AppName}-ecr-repository-url')
+    ECRRepositoryName = ECRRepositoryURL.split("/")[-1]
+
+    PARAMETERS = [
+        {"ParameterKey": "AppName", "ParameterValue": AppName},
+        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
+        {"ParameterKey": "Environment", "ParameterValue": Environment},
+        {"ParameterKey": "AWSRegion", "ParameterValue": AWS_REGION},
+        {"ParameterKey": "CodeCommitRepo", "ParameterValue": CodeCommitRepo},
+        {"ParameterKey": "DockerHubUsername", "ParameterValue": DockerHubUsername},
+        {"ParameterKey": "DockerHubPassword", "ParameterValue": DockerHubPassword},
+        {"ParameterKey": "CodeBuildServiceRole", "ParameterValue": CodeBuildServiceRole},
+        {"ParameterKey": "ECRRepositoryURL", "ParameterValue": ECRRepositoryURL},
+        {"ParameterKey": "ECRRepositoryName", "ParameterValue": ECRRepositoryName}
+    ]
+
+    return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
+
 def CreateStacks():
     # Read config.json file
     with open('config.json') as config_file:
         config_data = json.load(config_file)
+
+    app_name = config_data.get('AppName')
 
     # Create CodeCommit and ECR Repositories
     resource_locators = {}
     resource_locators = CreateSourceStack(config_data, resource_locators)
 
     # Sync GitHub repositories to CodeCommit
-    SyncRepository(config_data.get('APIGitHubURL'), resource_locators.get('CodeCommitRepositoryURLAPI'))
-    SyncRepository(config_data.get('UIGitHubURL'), resource_locators.get('CodeCommitRepositoryURLUI'))
+    SyncRepository(config_data.get('APIGitHubURL'), resource_locators.get(f'{app_name}-api-cc-repository-url'))
+    SyncRepository(config_data.get('UIGitHubURL'), resource_locators.get(f'{app_name}-ui-cc-repository-url'))
 
     # Create Network Resources
     resource_locators = CreateNetworkStack(config_data, resource_locators)
 
     # Create Service Roles
     resource_locators = CreateServiceRolesStack(config_data, resource_locators)
+
+    # Create Build Stacks
+    resource_locators = CreateAPIBuildStack(config_data, resource_locators)
+    resource_locators = CreateUIBuildStack(config_data, resource_locators)
+
+    # Run Builds
+
+    # Create EC2 Server Stacks
+
+    # Create Fargate Stacks
+
+    # Create Pipeline Stacks
+
+    # Celebrate victory?
 
 CreateStacks()

e3306c0943cfcde8dbd8cc6b56849c04109e7f7e
Jonathon Reading <jonathonreading@gmail.com>
2024-02-08 15:27:49 -0700

    add CreateReverseProxyStack, start code cleanup for deploy script

--- Scripts/deploy_stacks.py
+++ Scripts/deploy_stacks.py
@@ -6,24 +6,24 @@ import time
 import pprint
 import getpass
 
-def CreateStack(stack_name, template_file, parameters, aws_profile, aws_region, resource_locators):
+def CreateStack(AWSProfile, AWSRegion, Parameters, ResourceLocators, StackName, TemplateFile):
     # Create the CloudFormation stack with parameters
-    print(f"Creating {stack_name}.")
-    client = boto3.client('cloudformation', region_name=aws_region)
-    os.environ['AWS_PROFILE'] = aws_profile
+    print(f"Creating {StackName}.")
+    client = boto3.client('cloudformation', region_name=AWSRegion)
+    os.environ['AWS_PROFILE'] = AWSProfile
     response = client.create_stack(
-        StackName=stack_name,
-        TemplateBody=open(template_file).read(),
+        StackName=StackName,
+        TemplateBody=open(TemplateFile).read(),
         Capabilities=['CAPABILITY_NAMED_IAM'],
-        Parameters=parameters
+        Parameters=Parameters
     )
 
     # Wait for stack creation to complete
     print("Waiting for stack creation to complete...")
-    client.get_waiter('stack_create_complete').wait(StackName=stack_name)
+    client.get_waiter('stack_create_complete').wait(StackName=StackName)
 
     # Check stack creation status
-    response = client.describe_stacks(StackName=stack_name)
+    response = client.describe_stacks(StackName=StackName)
     stack_status = response['Stacks'][0]['StackStatus']
 
     if stack_status == "CREATE_COMPLETE":
@@ -34,33 +34,33 @@ def CreateStack(stack_name, template_file, parameters, aws_profile, aws_region, 
         else:
             stack_outputs = []
         for output in stack_outputs:
-            resource_locators[output['ExportName']] = output['OutputValue']
+            ResourceLocators[output['ExportName']] = output['OutputValue']
 
         print("Resource locators:")
-        pprint.pprint(resource_locators)
+        pprint.pprint(ResourceLocators)
     else:
         print(f"Stack creation failed. Status: {stack_status}")
 
-    return resource_locators
+    return ResourceLocators
 
-def GetSecretByARN(secret_arn):
+def GetSecretByARN(SecretARN):
     client = boto3.client('secretsmanager')
-    response = client.get_secret_value(SecretId=secret_arn)
+    response = client.get_secret_value(SecretId=SecretARN)
     secret_value = response['SecretString']
     return secret_value
 
-def PopulateCodeCommitRepository(github_repo_url, codecommit_repo_url):
+def PopulateCodeCommitRepository(CodeCommitRepoUrl, GithubRepoURL):
     # Clone GitHub repository
     import subprocess
-    subprocess.run(["git", "clone", github_repo_url])
+    subprocess.run(["git", "clone", GithubRepoURL])
 
     # Change directory to the cloned repository
     import os
-    repo_name = os.path.basename(github_repo_url).split('.')[0]
-    os.chdir(repo_name)
+    RepoName = os.path.basename(GithubRepoURL).split('.')[0]
+    os.chdir(RepoName)
 
     # Push to CodeCommit repository
-    subprocess.run(["git", "push", codecommit_repo_url, "--all"])
+    subprocess.run(["git", "push", CodeCommitRepoUrl, "--all"])
 
     # Navigate back to the parent directory
     os.chdir('..')
@@ -68,130 +68,124 @@ def PopulateCodeCommitRepository(github_repo_url, codecommit_repo_url):
     # Delete the local repository
     try:
         import shutil
-        shutil.rmtree(repo_name)
+        shutil.rmtree(RepoName)
     except:
-        print(f"Failed to delete the local repository: {repo_name}")
+        print(f"Failed to delete the local repository: {RepoName}")
 
     print("GitHub to CodeCommit sync completed.")
 
-def CreateSecretsStack(config_data, resource_locators):
-    # Configuration
-    AWS_PROFILE = "default"
-    AWS_REGION = "us-east-1"
-    STACK_NAME = f"{config_data.get('AppName')}-secrets-stack"
-    TEMPLATE_FILE = "../Config/secrets.yml"
+def CreateSecretsStack(ConfigData, ResourceLocators):
+    # From Config
+    AppName = ConfigData.get('AppName')
+    AWSProfile = ConfigData.get('AWSProfile')
+    AWSRegion = ConfigData.get('AWSRegion')
+    Environment = ConfigData.get('Environment')
+    ProjectName = ConfigData.get('ProjectName')
+    StackName = f"{ConfigData.get('AppName')}-secrets-stack"
+    TemplateFile = ConfigData.get('TemplateFilePaths').get('Secrets')
 
-    # Parameters
-    AppName = config_data.get('AppName')
-    ProjectName = config_data.get('ProjectName')
-    Environment = config_data.get('Environment')
+    # From User Input
     DockerUsername = input("Enter DockerHub username: ")
     DockerPassword = getpass.getpass("Enter DockerHub password: ")
     DatabaseUsername = input("Enter database username: ")
     DatabasePassword = getpass.getpass("Enter database password: ")
 
-    PARAMETERS = [
+    Parameters = [
         {"ParameterKey": "AppName", "ParameterValue": AppName},
-        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
-        {"ParameterKey": "Environment", "ParameterValue": Environment},
-        {"ParameterKey": "DockerUsername", "ParameterValue": DockerUsername},
-        {"ParameterKey": "DockerPassword", "ParameterValue": DockerPassword},
         {"ParameterKey": "DatabaseUsername", "ParameterValue": DatabaseUsername},
-        {"ParameterKey": "DatabasePassword", "ParameterValue": DatabasePassword}
-    ]
-
-    return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
-
-def CreateSourceStack(config_data, resource_locators):
-    # Configuration
-    AWS_PROFILE = "default"
-    AWS_REGION = "us-east-1"
-    STACK_NAME = f"{config_data.get('AppName')}-source-stack"
-    TEMPLATE_FILE = "../Pipelines/source.yml"
-
-    # Parameters
-    AppName = config_data.get('AppName')
-    ProjectName = config_data.get('ProjectName')
-    Environment = config_data.get('Environment')
-
-    PARAMETERS = [
-        {"ParameterKey": "AppName", "ParameterValue": AppName},
-        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
-        {"ParameterKey": "Environment", "ParameterValue": Environment}
-    ]
-
-    return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
-
-def CreateNetworkStack(config_data, resource_locators):
-    # Configuration
-    AWS_PROFILE = "default"
-    AWS_REGION = "us-east-1"
-    STACK_NAME = f"{config_data.get('AppName')}-network-stack"
-    TEMPLATE_FILE = "../Config/network.yml"
-
-    # Parameters
-    AppName = config_data.get('AppName')
-    ProjectName = config_data.get('ProjectName')
-    Environment = config_data.get('Environment')
-    IngressCIDR = f'{config_data.get('IngressIP')}/32'
-
-    PARAMETERS = [
-        {"ParameterKey": "AppName", "ParameterValue": AppName},
-        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
+        {"ParameterKey": "DatabasePassword", "ParameterValue": DatabasePassword},
+        {"ParameterKey": "DockerHubUsername", "ParameterValue": DockerUsername},
+        {"ParameterKey": "DockerHubPassword", "ParameterValue": DockerPassword},
         {"ParameterKey": "Environment", "ParameterValue": Environment},
-        {"ParameterKey": "IngressCIDR", "ParameterValue": IngressCIDR}
+        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName}
     ]
 
-    return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
+    return CreateStack(AWSProfile, AWSRegion, Parameters, ResourceLocators, StackName, TemplateFile)
 
-def CreateServiceRolesStack(config_data, resource_locators):
-    # Configuration
-    AWS_PROFILE = "default"
-    AWS_REGION = "us-east-1"
-    STACK_NAME = f"{config_data.get('AppName')}-service-role-stack"
-    TEMPLATE_FILE = "../Config/serviceroles.yml"
+def CreateSourceStack(ConfigData, ResourceLocators):
+    # From Config
+    AppName = ConfigData.get('AppName')
+    AWSProfile = ConfigData.get('AWSProfile')
+    AWSRegion = ConfigData.get('AWSRegion')
+    Environment = ConfigData.get('Environment')
+    ProjectName = ConfigData.get('ProjectName')
+    StackName = f"{ConfigData.get('AppName')}-source-stack"
+    TemplateFile = ConfigData.get('TemplateFilePaths').get('Source')
 
-    # Parameters
-    AppName = config_data.get('AppName')
-    ProjectName = config_data.get('ProjectName')
-    Environment = config_data.get('Environment')
-
-    PARAMETERS = [
+    Parameters = [
         {"ParameterKey": "AppName", "ParameterValue": AppName},
-        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
-        {"ParameterKey": "Environment", "ParameterValue": Environment}
+        {"ParameterKey": "Environment", "ParameterValue": Environment},
+        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName}
     ]
 
-    return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
+    return CreateStack(AWSProfile, AWSRegion, Parameters, ResourceLocators, StackName, TemplateFile)
 
-def CreateAPIBuildStack(config_data, resource_locators):
-    # Configuration
-    AWS_PROFILE = "default"
-    AWS_REGION = "us-east-1"
-    STACK_NAME = f"{config_data.get('AppName')}-api-build-stack"
-    TEMPLATE_FILE = "../Pipelines/build.yml"
+def CreateNetworkStack(ConfigData, ResourceLocators):
+    # From Config
+    AppName = ConfigData.get('AppName')
+    AWSProfile = ConfigData.get('AWSProfile')
+    AWSRegion = ConfigData.get('AWSRegion')
+    Environment = ConfigData.get('Environment')
+    IngressCIDR = f"{ConfigData.get('IngressIP')}/32"
+    ProjectName = ConfigData.get('ProjectName')
+    StackName = f"{ConfigData.get('AppName')}-network-stack"
+    TemplateFile = ConfigData.get('TemplateFilePaths').get('Network')
 
-    # Parameters
-    AppName = f"{config_data.get('AppName')}-api"
-    ProjectName = config_data.get('ProjectName')
-    Environment = config_data.get('Environment')
+    Parameters = [
+        {"ParameterKey": "AppName", "ParameterValue": AppName},
+        {"ParameterKey": "Environment", "ParameterValue": Environment},
+        {"ParameterKey": "IngressCIDR", "ParameterValue": IngressCIDR},
+        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName}
+    ]
 
-    DockerSecretARN = resource_locators.get(f'{config_data.get('AppName')}-docker-secret-arn')
-    secretvalue = GetSecretByARN(DockerSecretARN)
-    secretvalue = json.loads(secretvalue)
-    DockerHubUsername = secretvalue.get('username')
-    DockerHubPassword = secretvalue.get('password')
-        
-    CodeCommitRepo = resource_locators.get(f'{AppName}-cc-repository-url')
-    CodeBuildServiceRole = resource_locators.get(f'{config_data.get('AppName')}-codebuild-service-role-arn')
-    ECRRepositoryURL = resource_locators.get(f'{AppName}-ecr-repository-url')
+    return CreateStack(AWSProfile, AWSRegion, Parameters, ResourceLocators, StackName, TemplateFile)
+
+def CreateServiceRolesStack(ConfigData, ResourceLocators):
+    # From Config
+    AppName = ConfigData.get('AppName')
+    AWSProfile = ConfigData.get('AWSProfile')
+    AWSRegion = ConfigData.get('AWSRegion')
+    Environment = ConfigData.get('Environment')
+    ProjectName = ConfigData.get('ProjectName')
+    StackName = f"{ConfigData.get('AppName')}-service-role-stack"
+    TemplateFile = ConfigData.get('TemplateFilePaths').get('ServiceRoles')
+
+    Parameters = [
+        {"ParameterKey": "AppName", "ParameterValue": AppName},
+        {"ParameterKey": "Environment", "ParameterValue": Environment},
+        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName}
+    ]
+
+    return CreateStack(AWSProfile, AWSRegion, Parameters, ResourceLocators, StackName, TemplateFile)
+
+def CreateAPIBuildStack(ConfigData, ResourceLocators):
+    # From Config
+    AppName = f"{ConfigData.get('AppName')}-api"
+    AWSProfile = ConfigData.get('AWSProfile')
+    AWSRegion = ConfigData.get('AWSRegion')
+    Environment = ConfigData.get('Environment')
+    ProjectName = ConfigData.get('ProjectName')
+    StackName = f"{ConfigData.get('AppName')}-api-build-stack"
+    TemplateFile = ConfigData.get('TemplateFilePaths').get('Build')
+
+    # From Resource Locators
+    CodeBuildServiceRole = ResourceLocators.get(f'{ConfigData.get('AppName')}-codebuild-service-role-arn')
+    CodeCommitRepo = ResourceLocators.get(f'{AppName}-cc-repository-url')
+    ECRRepositoryURL = ResourceLocators.get(f'{AppName}-ecr-repository-url')
     ECRRepositoryName = ECRRepositoryURL.split("/")[-1]
 
-    PARAMETERS = [
+    # From Secrets Manager
+    DockerSecretARN = ResourceLocators.get(f'{ConfigData.get('AppName')}-docker-secret-arn')
+    SecretValue = GetSecretByARN(DockerSecretARN)
+    SecretValue = json.loads(SecretValue)
+    DockerHubUsername = SecretValue.get('username')
+    DockerHubPassword = SecretValue.get('password')
+
+    Parameters = [
         {"ParameterKey": "AppName", "ParameterValue": AppName},
         {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
         {"ParameterKey": "Environment", "ParameterValue": Environment},
-        {"ParameterKey": "AWSRegion", "ParameterValue": AWS_REGION},
+        {"ParameterKey": "AWSRegion", "ParameterValue": AWSRegion},
         {"ParameterKey": "CodeCommitRepo", "ParameterValue": CodeCommitRepo},
         {"ParameterKey": "DockerHubUsername", "ParameterValue": DockerHubUsername},
         {"ParameterKey": "DockerHubPassword", "ParameterValue": DockerHubPassword},
@@ -200,7 +194,7 @@ def CreateAPIBuildStack(config_data, resource_locators):
         {"ParameterKey": "ECRRepositoryName", "ParameterValue": ECRRepositoryName}
     ]
 
-    return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
+    return CreateStack(AWSProfile, AWSRegion, Parameters, ResourceLocators, StackName, TemplateFile)
 
 def CreateUIBuildStack(config_data, resource_locators):
     # Configuration
@@ -305,39 +299,75 @@ def CreateDatabaseStack(config_data, resource_locators):
 
     return CreateStack(STACK_NAME, TEMPLATE_FILE, PARAMETERS, AWS_PROFILE, AWS_REGION, resource_locators)
 
+def CreateReverseProxyStack(ConfigData, ResourceLocators):
+    # From Config
+    AMI = ConfigData.get('DebianAMI')
+    AppName = ConfigData.get('AppName')
+    AWSProfile = ConfigData.get('AWSProfile')
+    AWSRegion = ConfigData.get('AWSRegion')
+    Environment = ConfigData.get('Environment')
+    IngressCIDR = f"{ConfigData.get('IngressIP')}/32"
+    KeypairName = ConfigData.get('KeypairName')
+    ProjectName = ConfigData.get('ProjectName')
+    StackName = f"{ConfigData.get('AppName')}-reverse-proxy-stack"
+    TemplateFile = "../EC2/nginx.yml"
+
+    # From Resource Locators
+    APIElasticIP = ResourceLocators.get(f'{AppName}-api-elastic-ip')
+    ReverseProxyElasticIP = ResourceLocators.get(f'{AppName}-reverse-proxy-elastic-ip')
+    SecurityGroup = ResourceLocators.get(f'{AppName}-reverse-proxy-security-group-id')
+    SubnetId = ResourceLocators.get(f'{AppName}-reverse-proxy-subnet-id')
+    UIElasticIP = ResourceLocators.get(f'{AppName}-ui-elastic-ip')
+
+    Parameters = [
+        {"ParameterKey": "AMI", "ParameterValue": AMI},
+        {"ParameterKey": "AppName", "ParameterValue": AppName},
+        {"ParameterKey": "Environment", "ParameterValue": Environment},
+        {"ParameterKey": "IngressCIDR", "ParameterValue": IngressCIDR},
+        {"ParameterKey": "KeypairName", "ParameterValue": KeypairName},
+        {"ParameterKey": "ProjectName", "ParameterValue": ProjectName},
+        {"ParameterKey": "APIElasticIP", "ParameterValue": APIElasticIP},
+        {"ParameterKey": "ReverseProxyElasticIP", "ParameterValue": ReverseProxyElasticIP},
+        {"ParameterKey": "SecurityGroup", "ParameterValue": SecurityGroup},
+        {"ParameterKey": "SubnetId", "ParameterValue": SubnetId},
+        {"ParameterKey": "UIElasticIP", "ParameterValue": UIElasticIP},
+    ]
+
+    return CreateStack(AWSProfile, AWSRegion, Parameters, ResourceLocators, StackName, TemplateFile)
+
 def CreateStacks():
     # Set up configuration
-    with open('config.json') as config_file:
-        config_data = json.load(config_file)
-    app_name = config_data.get('AppName')
-    resource_locators = {}
+    with open('config.json') as ConfigFile:
+        ConfigData = json.load(ConfigFile)
+    AppName = ConfigData.get('AppName')
+    ResourceLocators = {}
 
     # Create Secrets Stack
-    resource_locators = CreateSecretsStack(config_data, resource_locators)
+    ResourceLocators = CreateSecretsStack(ConfigData, ResourceLocators)
 
     # Create CodeCommit and ECR Repositories
-    resource_locators = CreateSourceStack(config_data, resource_locators)
+    ResourceLocators = CreateSourceStack(ConfigData, ResourceLocators)
 
     # Sync GitHub repositories to CodeCommit
-    PopulateCodeCommitRepository(config_data.get('APIGitHubURL'), resource_locators.get(f'{app_name}-api-cc-repository-url'))
-    PopulateCodeCommitRepository(config_data.get('UIGitHubURL'), resource_locators.get(f'{app_name}-ui-cc-repository-url'))
+    PopulateCodeCommitRepository(ConfigData.get('APIGitHubURL'), ResourceLocators.get(f'{AppName}-api-cc-repository-url'))
+    PopulateCodeCommitRepository(ConfigData.get('UIGitHubURL'), ResourceLocators.get(f'{AppName}-ui-cc-repository-url'))
 
     # Create Network Resources
-    resource_locators = CreateNetworkStack(config_data, resource_locators)
+    ResourceLocators = CreateNetworkStack(ConfigData, ResourceLocators)
 
     # Create Service Roles
-    resource_locators = CreateServiceRolesStack(config_data, resource_locators)
+    ResourceLocators = CreateServiceRolesStack(ConfigData, ResourceLocators)
 
     # Create Build Stacks
-    resource_locators = CreateAPIBuildStack(config_data, resource_locators)
-    resource_locators = CreateUIBuildStack(config_data, resource_locators)
+    ResourceLocators = CreateAPIBuildStack(ConfigData, ResourceLocators)
+    ResourceLocators = CreateUIBuildStack(ConfigData, ResourceLocators)
 
     # Run Builds
-    RunCodeBuildProject(resource_locators.get(f'{app_name}-api-codebuild-project-arn'))
-    RunCodeBuildProject(resource_locators.get(f'{app_name}-ui-codebuild-project-arn'))
+    RunCodeBuildProject(ResourceLocators.get(f'{AppName}-api-codebuild-project-arn'))
+    RunCodeBuildProject(ResourceLocators.get(f'{AppName}-ui-codebuild-project-arn'))
 
     # Create EC2 Server Stacks
-    resource_locators = CreateDatabaseStack(config_data, resource_locators)
+    ResourceLocators = CreateDatabaseStack(ConfigData, ResourceLocators)
 
     # Create Fargate Stacks
 
